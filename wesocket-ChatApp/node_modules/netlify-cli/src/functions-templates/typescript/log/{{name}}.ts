6d6f2c7132f3c04b3a4c6178fb4514d25780e827
satyam0337 <satyam.kyp.lkr@gmail.com>
2022-12-10 23:47:55 +0530

    add

--- wesocket-ChatApp/node_modules/netlify-cli/src/functions-templates/typescript/log/{{name}}.ts
+++
@@ -1,9 +0,0 @@
-import type { Context } from "https://edge.netlify.com";
-
-export default async (request: Request, context: Context) => {
-  context.log("Hello from the logging service");
-
-  return new Response("The request to this URL was logged", {
-    headers: { "content-type": "text/html" },
-  });
-};
